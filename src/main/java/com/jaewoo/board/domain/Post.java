be71e289c3482f8208d005488df8eb64d10579eb
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-16 14:55:14 +0900

    feat: Post 어노테이션 추가

--- src/main/java/com/jaewoo/board/domain/Post.java
+++ src/main/java/com/jaewoo/board/domain/Post.java
@@ -2,6 +2,7 @@ package com.jaewoo.board.domain;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,8 +10,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Post {
 /*
  * 게시글
