dae25056b78511d55ca1603bf315c3cd188f20a5
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-16 14:45:28 +0900

    feat: Post, Content 도메인 작성

--- src/main/java/com/jaewoo/board/domain/Content.java
+++ src/main/java/com/jaewoo/board/domain/Content.java
@@ -1,5 +1,14 @@
 package com.jaewoo.board.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+
 public class Content {
 /*
  * 댓글
@@ -9,4 +18,21 @@ public class Content {
  * user(작성한 유저),
  * date정보(작성, 수정)
  */
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "content_id")
+	private Long id;
+	private String content;
+	private Post post;
+	private User user;
+	private LocalDateTime createDate;
+	private LocalDateTime updateDate;
+	
+	@Builder
+	public Content(String content, Post post, User user, LocalDateTime createDate, LocalDateTime updateDate) {
+		this.content = content;
+		this.post = post;
+		this.user = user;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+	}
 }
--- src/main/java/com/jaewoo/board/domain/Post.java
+++ src/main/java/com/jaewoo/board/domain/Post.java
@@ -1,5 +1,13 @@
 package com.jaewoo.board.domain;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+
 public class Post {
 /*
  * 게시글
@@ -9,4 +17,21 @@ public class Post {
  * user(작성한 유저),
  * date정보(작성, 수정)
  */
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private Long id;
+	private String title;
+	private String content;
+	private User user;
+	private LocalDateTime createDate;
+	private LocalDateTime updateDate;
+	
+	@Builder
+	public Post(String title, String content, User user, LocalDateTime createDate, LocalDateTime updateDate) {
+		this.title = title;
+		this.content = content;
+		this.user = user;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+	}
 }
