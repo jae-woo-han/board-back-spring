a21c98a82c966d2c891dcfe763253cdc380e24ac
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-18 00:11:47 +0900

    feat: 토큰 생성 매서드 추가

--- src/main/java/com/jaewoo/board/common/auth/jwt/JwtTokenProvider.java
+++ src/main/java/com/jaewoo/board/common/auth/jwt/JwtTokenProvider.java
@@ -1,15 +1,41 @@
 package com.jaewoo.board.common.auth.jwt;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenProvider {
 	private String secretKey;
-	private long validInMilliseconds;
+	private long validityInMilliseconds;
+	private Key key;
 	
-	public JwtTokenProvider(String secretKey, long validInMilliseconds) {
-		this.secretKey = secretKey;
-		this.validInMilliseconds = validInMilliseconds;
+	public JwtTokenProvider(String secretKey, long validityInMilliseconds) {
+		this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+		this.validityInMilliseconds = validityInMilliseconds;
+		this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+	}
+	
+	public String createToken(String subject) {
+		Claims claims = Jwts.claims().setSubject(subject);
+		
+		long now = (new Date()).getTime();
+		Date expirationTime = new Date(now+ this.validityInMilliseconds);
+		
+		return Jwts.builder()
+				.setClaims(claims)
+				.setExpiration(expirationTime)
+				.signWith(key, SignatureAlgorithm.HS256)
+				.compact();
 	}
 	
 	
