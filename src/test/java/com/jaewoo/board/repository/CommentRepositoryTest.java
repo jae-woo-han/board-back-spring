3eef9bad4331ccbfea162a30f20c504cf5b5b09e
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-18 21:22:37 +0900

    test: 댓글 테스트

--- src/test/java/com/jaewoo/board/repository/CommentRepositoryTest.java
+++ src/test/java/com/jaewoo/board/repository/CommentRepositoryTest.java
@@ -2,10 +2,17 @@ package com.jaewoo.board.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.jaewoo.board.domain.Comment;
+import com.jaewoo.board.domain.Post;
+import com.jaewoo.board.domain.User;
 
 @SpringBootTest
 @Transactional
@@ -18,9 +25,43 @@ class CommentRepositoryTest {
 	@Autowired
 	CommentRepository commentRepository;
 	
+	User user;
+	Post post;
+	
+	//유저와 게시글 생성
+	@BeforeEach
+	void 사전생성() {
+		User user = User
+				.builder()
+				.username("한재우")
+				.password("123456")
+				.nickname("째우")
+				.build();
+		this.user = userRepository.save(user);
+		Post post = Post.builder()
+				.user(user)
+				.title("제목")
+				.content("내용")
+				.createDate(LocalDateTime.now())
+				.updateDate(null)
+				.build();
+		this.post = postRepository.save(post);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void 댓글등록() {
+		Comment comment = Comment.builder()
+					.content("댓댓구")
+					.post(post)
+					.user(user)
+					.createDate(LocalDateTime.now())
+					.build();
+		Comment savedComment = commentRepository.save(comment);
+		assertSame(comment, savedComment, "객체불일치");
+	}
+	@Test
+	void 댓글수정() {
+		
 	}
 
 }
