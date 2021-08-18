b8e5b1dc2dec5ed078fb54e7f40a254e5ef50329
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-18 16:38:23 +0900

    feat: Jwt 토큰 provider 나머지 메소드 및 설정

--- src/main/java/com/jaewoo/board/common/auth/jwt/JwtTokenProvider.java
+++ src/main/java/com/jaewoo/board/common/auth/jwt/JwtTokenProvider.java
@@ -8,10 +8,12 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -24,7 +26,10 @@ public class JwtTokenProvider implements InitializingBean {
 	private long tokenValidityInMilliseconds;
 	private Key key;
 	
-	public JwtTokenProvider(String secretKey, long tokenValidityInMilliseconds) {
+	public JwtTokenProvider(
+			@Value("${jwt.secret}") String secretKey, 
+			@Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds
+			) {
 		this.secretKey = secretKey;
 		this.tokenValidityInMilliseconds = tokenValidityInMilliseconds;
 	}
@@ -34,6 +39,7 @@ public class JwtTokenProvider implements InitializingBean {
 		byte[] secretKeyBytes =secretKey.getBytes(StandardCharsets.UTF_8);
 		this.key = Keys.hmacShaKeyFor(secretKeyBytes);
 	}
+	
 	public String createToken(String subject) {
 		Claims claims = Jwts.claims().setSubject(subject);
 		
@@ -47,6 +53,21 @@ public class JwtTokenProvider implements InitializingBean {
 				.compact();
 	}
 
+	public String getSubject(String token) {
+		return Jwts.parserBuilder().setSigningKey(secretKey).build()
+				.parseClaimsJws(token).getBody().getSubject();
+	}
+	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (JwtException e) {//세부 예외 설정 필요
+			logger.info("JWT 예외 발생");
+			return false;
+		}
+	}
 	
 	
 	
