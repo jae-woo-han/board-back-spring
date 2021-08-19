495958aa0c82d2cc34891fd4aee8885f418bdf75
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-19 19:25:36 +0900

    feat: 게시글 등록 기능 추가

--- src/main/java/com/jaewoo/board/controller/PostController.java
+++ src/main/java/com/jaewoo/board/controller/PostController.java
@@ -1,8 +1,23 @@
 package com.jaewoo.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.Data;
 
 @Controller
 public class PostController {
 
+	@GetMapping("/posts/new")
+	public String moveNewPostPage(Model model) {
+		model.addAttribute("form", new PostForm());
+		return "post/createPostForm.html";
+	}
+	
+	@Data
+	class PostForm{
+		String title;
+		String content;
+	}
 }
