4820a3dde15c8f8a80ca7a484d8da7f59e893da5
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-20 15:40:51 +0900

    feat: ckeditor 이용해서 controller에 요청보내는 기능추가
    조사는 해봐야하는데
    ajax형식말고 일반적인 form으로 통신할 때는 리퀘스트 객체가 바깥에 있어야 스프링이 참조를 할수 있는 것같음

--- src/main/java/com/jaewoo/board/controller/PostController.java
+++ src/main/java/com/jaewoo/board/controller/PostController.java
@@ -3,21 +3,26 @@ package com.jaewoo.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import lombok.Data;
+import com.jaewoo.board.dto.PostFormDto;
+
 
 @Controller
 public class PostController {
 
 	@GetMapping("/posts/new")
 	public String moveNewPostPage(Model model) {
-		model.addAttribute("form", new PostForm());
+		model.addAttribute("form", new PostFormDto());
 		return "post/createPostForm.html";
 	}
 	
-	@Data
-	class PostForm{
-		String title;
-		String content;
+	@PostMapping("/posts/new")
+	public String saveNewPost(@ModelAttribute("form") PostFormDto form) {
+		System.out.println(form.toString());
+		
+		return "redirect:/";
 	}
+	
 }
---
+++ src/main/java/com/jaewoo/board/dto/PostFormDto.java
@@ -0,0 +1,11 @@
+package com.jaewoo.board.dto;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+public class PostFormDto {
+		String title;
+		String content;
+}
