789e65938aadca066c26a4bfae463c59f5f1211f
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-20 16:51:19 +0900

    feat: username으로 조회기능추가

--- src/main/java/com/jaewoo/board/repository/UserRepository.java
+++ src/main/java/com/jaewoo/board/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.jaewoo.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jaewoo.board.domain.User;
@@ -8,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long>{
 /* 필요기능
  * 회원가입, 로그인, 조회, 전체조회
  */
+	Optional<User> findOneByUsername(String username);
 }
