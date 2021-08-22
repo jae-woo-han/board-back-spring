4c71dc47ff6470acd0c92c7aa24eb2d454f5d21b
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-23 00:14:47 +0900

    feat: 가입, 로그인 관련 컨트롤러 중간저장

--- src/main/java/com/jaewoo/board/controller/AuthController.java
+++ src/main/java/com/jaewoo/board/controller/AuthController.java
@@ -1,8 +1,20 @@
 package com.jaewoo.board.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jaewoo.board.domain.User;
 
 @RestController
 public class AuthController {
 
+	
+	@PostMapping("/signup")
+	public ResponseEntity<User> signup(){
+		
+		return ResponseEntity.ok(User.builder().build());
+	}
+	
+	
 }
