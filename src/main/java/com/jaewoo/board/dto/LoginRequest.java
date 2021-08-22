af0fa9fe096d3c6840200ad121b14d42a94fe31e
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-23 00:04:19 +0900

    feat: 로그인 관련 DTO 추가

---
+++ src/main/java/com/jaewoo/board/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.jaewoo.board.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+	String username;
+	String password;
+}
---
+++ src/main/java/com/jaewoo/board/dto/LoginResponse.java
@@ -0,0 +1,9 @@
+package com.jaewoo.board.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+	String username;
+	String nickname;
+}
