2f068fa8b079ea5d326f9035d8a2021caefeefd2
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-26 12:16:12 +0900

    feat: 게시판에 게시글 볼수있는 기능 추가
    개선중

--- src/main/java/com/jaewoo/board/controller/HomeController.java
+++ src/main/java/com/jaewoo/board/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.jaewoo.board.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jaewoo.board.domain.Post;
+import com.jaewoo.board.repository.PostRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		PageRequest pageRequest = PageRequest.of(0, 20);
+		Page<Post> posts = postRepository.findAll(pageRequest);
+		model.addAttribute("posts", posts);
+		
 		return "/home.html";
 	}
 }
