5b197767306c4bfd9943a9f5be21d5c8e572d15d
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-08-26 22:33:47 +0900

    feat: 페이징 기능 추가

--- src/main/java/com/jaewoo/board/controller/HomeController.java
+++ src/main/java/com/jaewoo/board/controller/HomeController.java
@@ -4,9 +4,11 @@ package com.jaewoo.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jaewoo.board.domain.Post;
 import com.jaewoo.board.dto.PostPageResponse;
@@ -19,8 +21,8 @@ public class HomeController {
 	private PostRepository postRepository;
 	
 	@GetMapping("/")
-	public String home(Model model) {
-		PageRequest pageRequest = PageRequest.of(0, 20);
+	public String home(Model model, @RequestParam(name = "page", defaultValue = "0") Integer page) {
+		PageRequest pageRequest = PageRequest.of(page, 20, Sort.by(Sort.Direction.DESC, "id"));
 		Page<Post> posts = postRepository.findAll(pageRequest);
 		Page<PostPageResponse> postPage = posts.map(m -> {
 			PostPageResponse res = new PostPageResponse();
