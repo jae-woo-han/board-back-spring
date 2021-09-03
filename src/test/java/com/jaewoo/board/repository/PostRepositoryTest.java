966e26b14d1bb7d194563908c463ea6daf051b45
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-09-03 12:38:56 +0900

    test: Post 도메인 장문 설정 관련하여 테스트 코드 수정

--- src/test/java/com/jaewoo/board/repository/PostRepositoryTest.java
+++ src/test/java/com/jaewoo/board/repository/PostRepositoryTest.java
@@ -40,7 +40,20 @@ class PostRepositoryTest {
 		Post post = Post.builder()
 					.user(user)
 					.title("제목")
-					.content("내용")
+					.content("Manual testing is not what it was five years ago.\r\n"
+							+ "\r\n"
+							+ "The number of pure manual testing jobs is low and when such a job is available, the competition is fierce. It is common these days to meet manual testers that have a difficult time finding a new job.\r\n"
+							+ "\r\n"
+							+ "Due to budget restrictions for the testing team, there are also cases where manual testers are let go, but test automation engineers retained.\r\n"
+							+ "\r\n"
+							+ "Which doesn't make sense since developers are not great testers.\r\n"
+							+ "On the other hand, it is easier for developers to learn testing than it is for testers to learn development.\r\n"
+							+ "\r\n"
+							+ "Considering the market situation, many manual testers are now becoming more interested in test automation.\r\n"
+							+ "\r\n"
+							+ "Some want to just expand their skills by including programming and a test automation framework in their testing toolbox. Others are contemplating switching the direction completely and focusing on automation only.\r\n"
+							+ "\r\n"
+							+ "But how can you find that next automation job when you do not know programming?")
 					.createDate(LocalDateTime.now())
 					.updateDate(null)
 					.build();
